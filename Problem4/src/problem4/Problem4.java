a25cfb46da8ed79740660c781ba0560c738f8799
Chris Gallinaro <cgallinaro@ea.com>
2017-05-01 23:43:48 -0400

    Created empty projects for the 6 Problems

---
+++ Problem4/src/problem4/Problem4.java
@@ -0,0 +1,16 @@
+package problem4;
+
+/**
+ *
+ * @author cgallinaro
+ */
+public class Problem4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
