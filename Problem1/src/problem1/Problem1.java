17766c533620373829b658c59c8f4a679048e448
Chris Gallinaro <cgallinaro@ea.com>
2017-05-02 00:05:23 -0400

    Implemented Problem1

--- Problem1/src/problem1/Problem1.java
+++ Problem1/src/problem1/Problem1.java
@@ -10,7 +10,25 @@ public class Problem1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner keyboard = new Scanner(System.in);
+        
+        double number1, number2
+        
+        System.out.println("Enter the first number:");
+        number1 = keyboard.nextDouble();
+        System.out.println("Enter the second number:");
+        number2 = keyboard.nextBoolean();
+        
+        if (numberl > 0 && number2 > O) {
+            int sum = number1 + number2;
+            double average = sum / 2;
+
+            System.out.println("The sum is: + sum);
+            System.out.println("The average is: " + average);
+        else {
+            System.out.println("GOOD BYE!");
+        }
+                
     }
     
 }
