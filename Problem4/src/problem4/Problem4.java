3d3c2d245c926c96770e0155f4bf77c2ef1d3c77
manxboy <manxboytheboy@gmail.com>
2019-11-29 11:36:49 -0500

    projects completed

--- Problem4/src/problem4/Problem4.java
+++ Problem4/src/problem4/Problem4.java
@@ -1,5 +1,7 @@
 package problem4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cgallinaro
@@ -10,7 +12,49 @@ public class Problem4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        //prompt user
+        System.out.print("Type a sentence: ");
+        
+        //get the scentance
+        String original = scanner.nextLine();
+        
+        //remove punctuation
+        original = original.replaceAll("([?!\"':;().,-])", "");
+        
+        //remove spaces
+        original = original.replace(" ", "");
+        
+        //remove caps
+        original = original.toLowerCase();
+        
+        //reverse
+        String reversed = reverse(original);
+        
+        ///debug
+        if (original.equals(reversed)) {
+            System.out.println("This is a palindrome");
+        } else {
+            System.out.println("This is NOT a palindrome");
+        }
+    }
+    
+    /**
+     * Reverses a string by using a char sequence to conserve memory
+     * @param input
+     * @return 
+     */
+    public static String reverse(String input) {
+        char[] output = new char[input.length()];
+        
+        //ittorate through the string backwards and the char[] forwards, setting the char[] at the index equal the the string at the index
+        for (int CharPos=0, StringPos = input.length() - 1; StringPos >= 0; CharPos++, StringPos--) {
+            output[CharPos] = input.charAt(StringPos);
+        }
+        
+        return new String(output);
     }
     
 }
--- Problem6/src/problem6/Problem6.java
+++ Problem6/src/problem6/Problem6.java
@@ -1,5 +1,9 @@
 package problem6;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author cgallinaro
@@ -10,7 +14,35 @@ public class Problem6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        String input = scanner.nextLine();
+        
+        StringTokenizer tokenizer = new StringTokenizer(input);
+        
+        String finalOutput = "";
+        String currentLine = "";
+        
+        while (tokenizer.hasMoreTokens()) {
+            
+            String token = tokenizer.nextToken();
+            
+            //validate input
+            if (token.length() > 16) {
+                System.out.println("The input includes " + token + ", which is longer than 16 characters.");
+                return;
+            }
+            
+            
+            if ((currentLine + " " + token).length() > 16) {
+                finalOutput += "\n" + currentLine;
+                currentLine = token;
+            } else {
+                currentLine += " " + token;
+            }
+        }
+        
+        System.out.println(finalOutput);
     }
     
 }
